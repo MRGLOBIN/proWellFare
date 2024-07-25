545dbc1176bd78d9c2fb2d341e06c028ac4b8923
Muhammad Asim <asimqwe2@gmail.com>
2024-07-25 14:21:00 +0500

    add: filter for rpm status

--- src/app/setup/users/patients/data-grid-columns.tsx
+++ src/app/setup/users/patients/data-grid-columns.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Image from 'next/image'
 
 import { GridColDef } from '@mui/x-data-grid'
@@ -5,6 +7,21 @@ import { GridColDef } from '@mui/x-data-grid'
 import { ActivationStatus } from '@/app/types/data-grid-rows/patients.type'
 
 import { GridRenderCellParams } from '@mui/x-data-grid'
+
+import Button from '@mui/material/Button'
+
+import {
+  Checkbox,
+  Divider,
+  FormControlLabel,
+  IconButton,
+  Menu,
+  MenuItem,
+  MenuList,
+  Tooltip,
+} from '@mui/material'
+import { FilterList } from '@mui/icons-material'
+import { SetStateAction, useState } from 'react'
 
 // TODO: color for some properties are not assigned
 const statusCellBackgroundColourMapping = {
@@ -59,6 +76,117 @@ const rpmStatusCellRender = ({
     >
       {status}
     </span>
+  )
+}
+
+// TODO:
+// move to another file
+const RpmStatusColumnFilter = () => {
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null)
+  const [filterValues, setFilterValues] = useState<{ [key: string]: boolean }>({
+    Referred: false,
+    Terminated: false,
+    'Device Assigned': false,
+    Active: false,
+    Inactive: false,
+    'Training Scheduled': false,
+    'Did Not Answer': false,
+    Accepted: false,
+    Declined: false,
+    'Insurance verified': false,
+  })
+
+  const handleClick = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  const handleFilterChange = (event: {
+    target: { name: any; checked: any }
+  }) => {
+    setFilterValues({
+      ...filterValues,
+      [event.target.name]: event.target.checked,
+    })
+  }
+
+  return (
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <strong className='mr-3'>RPM Status</strong>
+      <Tooltip title='Filter'>
+        <IconButton
+          onClick={(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) =>
+            handleClick(event)
+          }
+          size='small'
+        >
+          <FilterList />
+        </IconButton>
+      </Tooltip>
+      <Menu
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+        slotProps={{
+          paper: {
+            style: {
+              height: '250px',
+              backgroundColor: '#2A2D38',
+              color: 'white',
+            },
+          },
+        }}
+      >
+        <div className='ml-3 mt-1'>
+          <Button
+            variant='outlined'
+            size='small'
+            className='mr-3'
+            sx={{
+              textTransform: 'none',
+              fontWeight: 900,
+              borderColor: '#444750',
+            }}
+          >
+            Select all
+          </Button>
+          <Button
+            variant='outlined'
+            size='small'
+            sx={{
+              textTransform: 'none',
+              fontWeight: 900,
+              borderColor: '#444750',
+            }}
+          >
+            Clear
+          </Button>
+        </div>
+        <div className='flex justify-center'>
+          <hr className='mt-2 w-[90%] border-[#1976d2]' />
+        </div>
+        {Object.keys(filterValues).map(key => (
+          <MenuItem key={key}>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={filterValues[key]}
+                  onChange={handleFilterChange}
+                  name={key}
+                  sx={{ color: 'white', fontSize: '12px' }}
+                />
+              }
+              label={key}
+            />
+          </MenuItem>
+        ))}
+      </Menu>
+    </div>
   )
 }
 
@@ -166,7 +294,9 @@ export const gridColumns: GridColDef[] = [
     flex: 2,
     type: 'string',
     headerClassName: 'bg-[#686868]',
+    sortable: false,
     renderCell: rpmStatusCellRender,
+    renderHeader: RpmStatusColumnFilter,
   },
   {
     field: 'comment',
@@ -224,3 +354,8 @@ export const gridColumns: GridColDef[] = [
     headerClassName: 'bg-[#686868]',
   },
 ]
+
+//  return (
+//         <div className='flex items-center'>
+//           <strong className='mx-2'>RPM Status</strong> <FilterListIcon />
+//         </div>
